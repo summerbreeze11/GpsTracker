5defdc78e860f899977c1cd20a9293e1c2ff6270
summerbreeze11 <wf992211@mail.ru>
2023-05-22 20:04:03 +0700

    Initial commit

---
+++ app/src/main/java/com/summerbreeze11/gpstracker/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.summerbreeze11.gpstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.summerbreeze11.gpstracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
