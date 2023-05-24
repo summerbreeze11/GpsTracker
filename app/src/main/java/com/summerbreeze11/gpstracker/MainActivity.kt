ab8c1ab78f238ffd72eb89b5ce7d1e975817c993
summerbreeze11 <wf992211@mail.ru>
2023-05-24 23:01:50 +0700

    Modify settings fragment

--- app/src/main/java/com/summerbreeze11/gpstracker/MainActivity.kt
+++ app/src/main/java/com/summerbreeze11/gpstracker/MainActivity.kt
@@ -2,13 +2,11 @@ package com.summerbreeze11.gpstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.summerbreeze11.gpstracker.databinding.ActivityMainBinding
 import com.summerbreeze11.gpstracker.fragments.MainFragment
 import com.summerbreeze11.gpstracker.fragments.SettingsFragment
 import com.summerbreeze11.gpstracker.fragments.TracksFragment
 import com.summerbreeze11.gpstracker.utils.openFragment
-import com.summerbreeze11.gpstracker.utils.showToast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -31,7 +29,7 @@ class MainActivity : AppCompatActivity() {
                     openFragment(TracksFragment.newInstance())
                 }
                 R.id.id_settings -> {
-                    openFragment(SettingsFragment.newInstance())
+                    openFragment(SettingsFragment())
                 }
             }
             true
--- app/src/main/java/com/summerbreeze11/gpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/com/summerbreeze11/gpstracker/fragments/SettingsFragment.kt
@@ -1,25 +1,57 @@
 package com.summerbreeze11.gpstracker.fragments
 
+import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.summerbreeze11.gpstracker.databinding.FragmentSettingsBinding
+import androidx.preference.Preference
+import androidx.preference.Preference.OnPreferenceChangeListener
+import androidx.preference.PreferenceFragmentCompat
+import com.summerbreeze11.gpstracker.R
 
-class SettingsFragment : Fragment() {
-    private lateinit var binding: FragmentSettingsBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.root
+class SettingsFragment : PreferenceFragmentCompat() {
+    private lateinit var timePref: Preference
+    private lateinit var colorPref: Preference
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.main_preference, rootKey)
+        init()
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = SettingsFragment()
+    private fun init() {
+        timePref = findPreference("update_time_key")!!
+        colorPref = findPreference("color_key")!!
+        val changeListener = onChangeListener()
+        timePref.onPreferenceChangeListener = changeListener
+        colorPref.onPreferenceChangeListener = changeListener
+        initPrefs()
+    }
+
+    private fun onChangeListener(): OnPreferenceChangeListener {
+        return Preference.OnPreferenceChangeListener { pref, value ->
+            when (pref.key) {
+                "update_time_key" -> onTimeChange(value.toString())
+                "color_key" -> pref.icon?.setTint(Color.parseColor(value.toString()))
+            }
+
+            true
+        }
+    }
+
+    private fun onTimeChange(value: String) {
+        val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+        val valueArray = resources.getStringArray(R.array.loc_time_update_values)
+        val title = timePref.title.toString().substringBefore(":")
+        timePref.title = "$title: ${nameArray[valueArray.indexOf(value)]}"
+    }
+
+    private fun initPrefs() {
+        val pref = timePref.preferenceManager.sharedPreferences
+        val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+        val valueArray = resources.getStringArray(R.array.loc_time_update_values)
+        val title = timePref.title
+        timePref.title =
+            "$title: ${nameArray[valueArray.indexOf(pref?.getString("update_time_key", "3000"))]}"
+
+        val trackColor = pref?.getString("color_key", "#001AFF")
+        colorPref.icon?.setTint(Color.parseColor(trackColor))
+
     }
 }
